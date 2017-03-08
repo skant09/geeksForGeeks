6a5557f37d24f9c67278d5fbc7103c82d5a0ec80
suryaZivame <surya@zivame.com>
2017-03-09 02:53:13 +0530

    bind and call testing

---
+++ javascript/bindCall.js
@@ -0,0 +1,19 @@
+'use strict';
+var a = {
+	name: 'surya',
+	say : function(){
+		console.log(this.name);
+	}
+}
+
+var func = a.say;
+console.log('func');
+func(); // error in strict node or undefined in normal mode
+a.say(); //surya
+a.say.bind({name:'mohan'}); // logs nothing i.e no execution
+console.log(a.say); //logs function
+console.log(a.say.call({name:'something'})); // logs 'something' \n 'undefined'
+a.say.call({name:'ravi'});
+a.say(); //surya
+a.say = a.say.bind({name:'mohan'});
+a.say();

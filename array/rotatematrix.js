148a3487d0c0c2c4602ad6bfeb90aec26a796342
skant09 <s.kant@outlook.com>
2018-11-21 13:27:15 +0530

    .gitignore added

--- array/rotatematrix.js
+++ array/rotatematrix.js
@@ -1,3 +1,5 @@
+var columnify = require("columnify");
+
 var squareMatrix = [
   [1, 2, 3, 4],
   [5, 6, 7, 8],
@@ -5,6 +7,7 @@ var squareMatrix = [
   [13, 14, 15, 16]
 ];
 squareMatrix = [[1, 2, 3], [4, 5, 6], [7, 8, 9]];
+// squareMatrix = [[1, 2, 3], [4, 5, 6], [7, 8, 9], [10, 11, 12]];
 
 function rotateCounterClock(matrix) {
   // validated that the matrix is square
@@ -30,6 +33,22 @@ function rotateClockWise(matrix) {
   });
   return rotatedMatrix;
 }
-console.log(rotateCounterClock(squareMatrix));
-console.log();
-console.log(rotateClockWise(squareMatrix));
+function rotate180(matrix) {
+  // validated that the matrix is square
+  const size = matrix.length;
+  let rotatedMatrix = [...new Array(size)].map(i => [[...new Array(size)]]);
+  matrix.forEach((row, rowIndex) => {
+    row.forEach((ele, columnIndex) => {
+      rotatedMatrix[rowIndex][columnIndex] =
+        matrix[size - 1 - columnIndex][size - 1 - rowIndex];
+    });
+  });
+  return rotatedMatrix;
+}
+console.log("squareMatrix \n", columnify(squareMatrix));
+console.log(
+  "rotateCounterClock \n",
+  columnify(rotateCounterClock(squareMatrix))
+);
+console.log("rotateClockWise \n", columnify(rotateClockWise(squareMatrix)));
+console.log("rotate180 \n", columnify(rotate180(squareMatrix)));
